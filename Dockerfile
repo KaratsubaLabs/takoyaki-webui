0af8b3cf59ba368d7debb235c984ec4d7e0d2ecf
bloodandcoffee <kugelblitz3@outlook.com>
2023-02-23 18:11:49 -0500

    reinit with ts and vite

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-
-FROM node:lts-alpine
-WORKDIR /app
-COPY package.json ./
-COPY package-lock.json ./
-RUN npm ci
-COPY . .
-CMD npm run build
-
--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,85 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div class="header">
-    <h2 class="logo headertext"><router-link class="link" to="/">タコヤキ</router-link></h2>
-    <p class="headertext marginfree">BY</p>
-    <a class="headertext link" href="https://github.com/KaratsubaLabs" target="_blank">からつばLABS</a>
-  </div>
-  <div class="body"><router-view /></div>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
-.header {
-  width: 100%;
-  text-align: left;
-  background-color: lightseagreen;
-}
-.body {
-  margin: 8px 0;
-}
+
 .logo {
-  color: #F0F0F0;
-  margin: 4px 8px !important;
+  display: block;
+  margin: 0 auto 2rem;
 }
-.headertext {
-  margin: 0 6px 0 6px;
-  display: inline-flex;
-  vertical-align: middle;
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
 }
-a.headertext, p.headertext {
-  color: #EEEEEE;
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
 }
-.link {
-  cursor: default;
-  text-decoration: none;
-  color: inherit;
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
 }
-.link:active {
-  color: lightseagreen;
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
 }
-.marginfree {
-  margin-right: 0px;
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
